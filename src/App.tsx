bc59735df4334f446ba9be30000517505748c704
majate <majate@kth.se>
2022-06-27 14:51:29 +0200

    Lazy loading of routes and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import FormController from './components/FormController'
-import Header from './components/Header'
+
+const FormController = React.lazy(() => import('./components/FormController'))
+const Header = React.lazy(() => import('./components/Header'))
 
 function App() {
   const menuItems = [
@@ -11,12 +12,16 @@ function App() {
 
   return (
     <Router>
-      <Header title="EXJOBB" titlePath="/" menuItems={menuItems} />
-      <Routes>
-        <Route path="/" element={<p>Home</p>} />
-        <Route path="form" element={<FormController />} />
-        <Route path="p2" element={<p>Page 2</p>} />
-      </Routes>
+      <Suspense fallback={null}>
+        <Header title="EXJOBB" titlePath="/" menuItems={menuItems} />
+      </Suspense>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<p>Home</p>} />
+          <Route path="form" element={<FormController />} />
+          <Route path="p2" element={<p>Page 2</p>} />
+        </Routes>
+      </Suspense>
     </Router>
   )
 }

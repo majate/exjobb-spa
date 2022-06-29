a3ed70cca44bf16e185e42c26b35aa0c3f66d88b
majate <majate@kth.se>
2022-06-30 00:06:09 +0200

    Fix ambiguous selectors

--- greenframe/scenario_navigate.js
+++ greenframe/scenario_navigate.js
@@ -3,13 +3,14 @@ async page => {
   await page.goto('', { waitUntil: 'networkidle' }) // Go to the baseUrl
   await page.waitForTimeout(3000) // Wait for 3 seconds
   const links = [
-    page.locator('button:has-text("Form")'),
-    page.locator('button:has-text("Feed")'),
-    page.locator('a:has-text("EXJOBB")'),
+    page.locator('button:has-text("Form"):visible'),
+    page.locator('button:has-text("Feed"):visible'),
+    page.locator('a:has-text("EXJOBB"):visible'),
   ]
   for (let i = 0; i < 3; i++) {
     for (const link of links) {
       await link.click()
+      await page.waitForTimeout(500)
       await page.waitForNetworkIdle()
     }
   }
